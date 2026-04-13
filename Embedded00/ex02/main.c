d56e98721f7391ce5ad1a09c304562176362f006
SimonIsCoding <sim.arch@proton.me>
2026-04-13 18:35:45 +0200

    ex02 done

---
+++ Embedded00/ex02/main.c
@@ -0,0 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRD &= ~(1 << DDD2);
+	PORTD |= (1 << PD2);
+	while (1)
+	{
+
+		if (!(PIND & (1 << PIND2)))
+		{
+			DDRB |= (1 << DDB0);
+			PORTB |= (1 << PORTB0);
+		}
+		else
+		{
+			DDRB &= (0 << DDB0);
+			PORTB &= (0 << PORTB);
+		}
+	}
+	return (0);
+}
