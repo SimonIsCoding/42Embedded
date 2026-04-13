3e6327db4034a9fb87860b55dc5b445215223a4d
SimonIsCoding <sim.arch@proton.me>
2026-04-13 16:30:28 +0200

    ex01 done

---
+++ Embedded00/ex01/main.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+
+/* 
+ * DDRB0: The D0 LED
+ * PORTB0: The intensity in Volt. 1 = 5V, 0 = 0V
+ initially, we have:
+	DDRB = 0b00000000;
+	PORTB = 0b00000000;
+ * DDRB |= (1 << DDB0) => We shift the bit to turn on the DDB0 LED
+ * We add 5V to PORTB to make the light brigths
+ * it became:
+	DDRB = 0b00000001;
+	PORTB = 0b00000001;
+*/
+
+int	main(void)
+{
+	DDRB |= (1 << DDB0);
+	PORTB |= (1 << PORTB0);
+
+	return (0);
+}
