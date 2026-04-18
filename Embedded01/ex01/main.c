b740c959718e2ae03c92a5c4c5d7051a3de48a9e
SimonIsCoding <sim.arch@proton.me>
2026-04-18 20:57:08 +0200

    reading datasheet and putting what Iunderstood

--- Embedded01/ex01/main.c
+++ Embedded01/ex01/main.c
@@ -7,9 +7,16 @@ int	main(void)
 	
 	volatile long i = 0;
 	DDRB |= (1 << DDB1);
-
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-
+	
+	// Refering to the ATMega328p Datasheet:
+	// fOCnx = fclk_I/O / (2 * N * (1 + OCRnx)) (p.156)
+	// knowing: FOCnx = 1Hz, fclk_I/O = 16*10e6, N = 1024
+	// OCRnx = 7812;
+	// OCR1A because we refer to Timer1 (p. 121)
+	OCR1A = 7812;                            // max timer value
+	// CTC Mode  (p.155)
+	// Activate WGM12 p.146 to reinitialise CTC when OCR1A is reached
+	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);	// register to configure TImer1
 	while(1)
 	{
 	}
